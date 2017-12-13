8e2af3697d3cb56a8304ca381bc7a04cbe2a3dcd
Animesh Pandey <animesh.pandey1903@rediffmail.com>
2017-12-13 21:27:08 +0530

    Added : Graphical Tiles

---
+++ Section 11 - Graphical Tiles/91. Load Your Tile Art in Code/3-load your tile art in code/Car.js
@@ -0,0 +1,59 @@
+var carPic = document.createElement("img");
+var carPicLoaded = false;
+
+var carX = 75;
+var carY = 75;
+var carAng = 0;
+var carSpeed = 0;
+
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.5;
+const REVERSE_POWER = 0.2;
+const TURN_RATE = 0.03;
+
+function carImageLoad() {
+	carPic.onload = function() {
+		carPicLoaded = true;
+	}
+	carPic.src = "player1car.png";
+}
+
+function carReset() {
+	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
+		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+			if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+				trackGrid[arrayIndex] = TRACK_ROAD;
+				carAng = -Math.PI/2;
+				carX = eachCol * TRACK_W + TRACK_W/2;
+				carY = eachRow * TRACK_H + TRACK_H/2;
+			}
+		}
+	}
+}
+
+function carMove() {
+	carSpeed *= GROUNDSPEED_DECAY_MULT;
+
+	if(keyHeld_Gas) {
+		carSpeed += DRIVE_POWER;
+	}
+	if(keyHeld_Reverse) {
+		carSpeed -= REVERSE_POWER;
+	}
+	if(keyHeld_TurnLeft) {
+		carAng -= TURN_RATE;
+	}
+	if(keyHeld_TurnRight) {
+		carAng += TURN_RATE;
+	}
+
+	carX += Math.cos(carAng) * carSpeed;
+	carY += Math.sin(carAng) * carSpeed;
+}
+
+function carDraw() {
+	if(carPicLoaded) {
+		drawBitmapCenteredWithRotation(carPic, carX,carY, carAng);
+	}
+}
